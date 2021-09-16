b17c95aa46d509235fad30381ebc858e73306f1c
yiperdemon <vpss3rv@gmail.com>
2021-09-16 22:43:30 +0530

    [+] Prototype implementation { Module whatsmyname }

--- whatsmyname.py
+++ whatsmyname.py
@@ -1,10 +1,43 @@
 import json
-
+import os
 import WhatsMyName.check_online_presence as wmn_cop
 import requests
 
-def check_username(user):
+if os.name == "posix":
+    class Colors:
+        YELLOW = "\033[93m"
+        RED = "\033[91m"
+        GREEN = "\033[92m"
+        ENDC = "\033[0m"
+else:
+    class Colors:
+        YELLOW = ""
+        RED = ""
+        GREEN = ""
+        ENDC = ""
 
+
+def warn(msg):
+    print(Colors.YELLOW + msg + Colors.ENDC)
+
+
+def error(msg):
+    print(Colors.RED + msg + Colors.ENDC)
+
+
+def positive(msg):
+    print(Colors.GREEN + msg + Colors.ENDC)
+
+
+def neutral(msg):
+    print(msg)
+
+
+def do_nothing(arg):
+    pass
+
+
+def check_username(user):
     # Suppress HTTPS warnings
     requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
 
@@ -17,6 +50,15 @@ def check_username(user):
 
     try:
         for site in sites_to_check:
-            pass
+            if site['valid']:
+                # INCOMPLETE
+                wmn_cop.check_site(site, user,
+                                   if_found=lambda url: positive("[+] User found at %s" % url),
+                                   if_not_found=lambda url: do_nothing(url),
+                                   if_neither=lambda url: error("[!] Error. The check implementation is broken "
+                                                                "for %s" % url))
     except:
         pass
+
+
+check_username('David')
