cfdb627a4122562fb92784847338f22f3c43daf1
HimanshuOtakuu <hs1925846@gmail.com>
2021-09-28 19:36:07 +0530

    instagram.py added

---
+++ packages/instagram.py
@@ -0,0 +1,56 @@
+import time
+from selenium import webdriver
+from bs4 import BeautifulSoup as BS
+
+
+def get_info(name: str, isurl: bool) -> dict:
+    """
+    This Function Filters the User's Instagram Profile
+    Useful Information on Github Profile:
+        1.  Login Name : userid
+        2.  Name: name
+        3. Bio: bio
+    Should Look:
+        1.  https://instagram.com/<userid>
+    :param isurl:
+    :param name:
+    :return info:
+    """
+
+    info = dict()
+    if not isurl:
+        plink = f"https://instagram.com/{name}"
+    else:
+        plink = name
+
+    # Firefox Driver (Selenium)
+    driver = webdriver.Firefox()
+    driver.get(plink)
+
+    # Approx Wait ( high speed internet required)
+    time.sleep(15)  # 15 Seconds Sleep
+
+    driver.close()
+
+    # Target Real Name
+    info['name'] = driver.find_element_by_class_name('rhpdm').string
+    # Target User ID
+    info['userid'] = driver.find_element_by_class_name('_7UhW9       fKFbl yUEEX   KV-D4              fDxYl  ').string
+    # Target Bio
+    info['bio'] = driver.find_element_by_class_name('-vDIg').string
+
+    return info
+
+
+def run(name: str, isurl: bool = False) -> dict:
+    """
+    Run Twitter Info Check
+    :param isurl:
+    :param name:
+    :return:
+    """
+    return get_info(name, isurl)
+
+
+if __name__ == '__main__':
+    print(get_info(input()))
