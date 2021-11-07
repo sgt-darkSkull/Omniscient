fa0a79cd1b6225a675fa0e663a5d1c8b93f8f09a
yiperdemon <vpss3rv@gmail.com>
2021-11-07 22:45:38 +0530

    sublist report update

--- pack_server/sublist_runscript.py
+++ pack_server/sublist_runscript.py
@@ -2,4 +2,8 @@ from pack_server import sublist3r
 
 
 def run(domain_name: str):
-    return sublist3r.main(domain_name, 30, None, None, True, False, False, None)
+    s_lst = ''
+    for sub in sublist3r.main(domain_name, 30, None, None, True, False, False, None):
+        s_lst += sub + '\n'
+
+    return s_lst
