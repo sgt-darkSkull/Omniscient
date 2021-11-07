9f4756960305aaedaf35048c7919f751388d50f1
yiperdemon <vpss3rv@gmail.com>
2021-11-07 22:46:20 +0530

    report update

--- pack_server/GoogleDork.py
+++ pack_server/GoogleDork.py
@@ -1,21 +1,23 @@
 def get_dorks(domain_name: str):
     from googlesearch import search
-    
-    info=list()
-    dork = domain_name
-    amount = 100
+
+    dorks = ''
+    tld = domain_name.split('.')[-1]
+    amount = 30
     interval = 2
     req = 0
 
-    for i in search(dork, tld="com", lang="en", num=int(amount), start=0, stop=None, pause=int(interval)):
-        info.append(i)
+    for i in search(domain_name, tld=tld, lang="en", num=int(amount), start=0, stop=30, pause=int(interval)):
+        dorks += dorks + i + '\n'
         req += 1
         if req >= int(amount):
             break
-        
-    return info
+    return dorks
 
 
 def run(domain_name: str):
     """returns list of the all the dorks"""
     return get_dorks(domain_name)
+
+if __name__ == '__main__':
+    print(run('arowex.com'))
